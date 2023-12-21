65433e7e1e7a067847b6ed8706589dc24ae468de
Nvl244 <77487344+Nvl244@users.noreply.github.com>
2023-12-21 16:43:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const Eris = require("eris");
 const keepAlive = require("./server");
 keepAlive();
 
-const bot = new Eris(process.env.TOKEN);
+const bot = new Eris(process.env.OTk5ODA1MzA0Nzc2Njg3NjM2.GDObMb.x12V1EWT8gJ1AersnNwcOsFBw0b_QaxstdbiX4);
 
 bot.connect();
